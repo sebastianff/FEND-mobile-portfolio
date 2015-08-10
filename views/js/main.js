f2a8e2265bd90054f2d07fd78a84764a5461077f
sebastianff <sebastianffarago@gmail.com>
2015-08-10 21:29:14 +0200

    Moved the pizzasDiv variable out of the lopp

--- views/js/main.js
+++ views/js/main.js
@@ -455,10 +455,9 @@ var resizePizzas = function(size) {
 };
 
 window.performance.mark("mark_start_generating"); // collect timing data
-
+pizzasDiv = document.getElementById("randomPizzas");
 // This for-loop actually creates and appends all of the pizzas when the page loads
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
